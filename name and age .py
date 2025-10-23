a37bc541830f523bec83a008a6f6cef2280d0e8d
Your Name <ahmadtheseen@gmail.com>
2025-10-23 19:49:18 +0300

    added assigment file

---
+++ name and age .py
@@ -0,0 +1,11 @@
+# Introduction to Programming with Python
+
+
+print("Hello, Computer Science!")
+
+# Asking user for  name and age
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+
+print(f"Hello {name}, you will be {age + 1} next year!")
